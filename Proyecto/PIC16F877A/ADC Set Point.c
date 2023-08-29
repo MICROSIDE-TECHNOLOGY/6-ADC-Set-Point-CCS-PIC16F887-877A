6116a5bdd3c2f9af83dec370ab881f77c394c15d
vhperaltaj <victor.peralta@microside.com>
2023-08-29 15:12:30 -0600

    Updated identation, added gitignore

--- Proyecto/PIC16F877A/ADC Set Point.c
+++ Proyecto/PIC16F877A/ADC Set Point.c
@@ -1,7 +1,12 @@
-/*
-AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
-FECHA: JUNIO 2019
-*/
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+ADC Set Point.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
 /*
 ---------------------------------------------------------------------------
@@ -10,45 +15,26 @@ un Set Point, el LED encenderá.
 ---------------------------------------------------------------------------
 */
 
-#include <16F877A.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#DEVICE ADC=8                                   //Configura el ADC a 8 bits
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F877A.h>                 //Incluye el microcontrolador con el que se va a trabajar 
+#DEVICE ADC=8                        //Configura el ADC a 8 bits
+#use delay ( clock=20Mhz, crystal )  //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
 
+#define LED PIN_A1                   //Pin donde está conectado el LED del X-TRAINER
 
-#define LED PIN_A1                              //Pin donde está conectado el LED del X-TRAINER
+int ValorPot;                        //Variable para almacenar el valor leído del ADC
 
-int ValorPot;                                   //Variable para almacenar el valor leído del ADC
+void main ( void ) {
+    setup_adc_ports ( AN0 );         //Configura solo el puerto A0 como entrada analógica
+    setup_adc ( ADC_CLOCK_DIV_8 );   //Configura oscilador para la conversión
 
-void SisInit(void) {
-    
-    setup_adc_ports (AN0);                         //Configura solo el puerto A0 como entrada analógica
+    while ( 1 ) {
+        set_adc_channel ( 0 );       //Selecciona el canal 0 para la conversión
+        ValorPot = read_adc ( );     //Lee el valor del ADC y lo guarda en la variable
 
-    setup_adc (ADC_CLOCK_DIV_8);                   //Configura oscilador para la conversión
-}
-
- 
-
-void main() {
-
-    SisInit();
-
-    while (1) {
-
-        set_adc_channel (0);                 //Selecciona el canal 0 para la conversión
-
-        ValorPot = read_adc ();              //Lee el valor del ADC y lo guarda en la variable
-
-        if (ValorPot >= 128) {                //Compara el valor de el ADC con 128
-
-            output_HIGH (LED);     // Leds activos
-
+        if ( ValorPot >= 128 ) {     //Compara el valor de el ADC con 128
+            output_HIGH ( LED );     // Leds activos
         } else {
-
-            output_LOW (LED);       // Leds activos
-
+            output_LOW ( LED );      // Leds activos
         }
-
     }
 }
-
- 
--- Proyecto/PIC16F887/ADC Set Point.c
+++ Proyecto/PIC16F887/ADC Set Point.c
@@ -1,7 +1,12 @@
-/*
-AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
-FECHA: JUNIO 2019
-*/
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+ADC Set Point.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
 /*
 ---------------------------------------------------------------------------
@@ -10,41 +15,27 @@ un Set Point, el LED encenderá.
 ---------------------------------------------------------------------------
 */
 
-#include <16F887.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#DEVICE ADC=8                                   //Configura el ADC a 8 bits
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F887.h>                  //Incluye el microcontrolador con el que se va a trabajar 
+#DEVICE ADC = 8                      //Configura el ADC a 8 bits
+#use delay ( clock=20Mhz, crystal )  //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
 
+#define LED PIN_A1                   //Pin donde está conectado el LED del X-TRAINER
 
-#define LED PIN_A1                              //Pin donde está conectado el LED del X-TRAINER
-
-int ValorPot;                                   //Variable para almacenar el valor leído del ADC
-
-void SisInit(void) {
-
-    setup_oscillator(OSC_8MHZ);                    //Configura oscilador interno
-
-    setup_adc_ports (sAN0);                         //Configura solo el puerto A0 como entrada analógica
-
-    setup_adc (ADC_CLOCK_DIV_8);                   //Configura oscilador para la conversión
-
-}
-
+int ValorPot;                        //Variable para almacenar el valor leído del ADC
  
-void main() {
-
-    SisInit();
+void main ( void ) {
+    setup_oscillator ( OSC_8MHZ );   //Configura oscilador interno
+    setup_adc_ports ( sAN0 );        //Configura solo el puerto A0 como entrada analógica
+    setup_adc ( ADC_CLOCK_DIV_8 );   //Configura oscilador para la conversión
 
     while (1) {
-        set_adc_channel (0);                 //Selecciona el canal 0 para la conversión
+        set_adc_channel ( 0 );       //Selecciona el canal 0 para la conversión
+        ValorPot = read_adc ( );     //Lee el valor del ADC y lo guarda en la variable
 
-        ValorPot = read_adc ();              //Lee el valor del ADC y lo guarda en la variable
-
-        if (ValorPot >= 128) {                //Compara el valor de el ADC con 128
-           output_HIGH (LED);     // Leds activos
-
+        if ( ValorPot >= 128 ) {     //Compara el valor de el ADC con 128
+           output_HIGH ( LED );      // Leds activos
         } else {
-           output_LOW (LED);       // Leds activos
-
+           output_LOW ( LED );       // Leds activos
         }
     }
 }
